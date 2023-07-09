43d543c0f12a0120f44fee1334642fcf02b67c46
Geovanny Gil <geovannygr2018@gmail.com>
2023-07-09 17:45:48 -0600

    chore: add template of eleven get started and config tailwind

---
+++ src/utils/app-config.js
@@ -0,0 +1,24 @@
+export const AppConfig = {
+  site_name: 'Astro boilerplate',
+  title: 'Astro boilerplate',
+  description: 'Boilerplate built with Astro using React and Tailwind CSS',
+  author: 'Creator',
+  theme_color: '#0b0b0b',
+  locale_region: 'es-GT',
+  locale: 'es'
+}
+
+export const OpenGraph = {
+  url: 'https://example.com',
+  type: 'website',
+  title: 'Astro boilerplate',
+  description: 'Boilerplate built with Astro using React and Tailwind CSS',
+  image: 'https://placehold.co/600x400.png'
+}
+
+export const TwitterCard = {
+  card: 'summary_large_image',
+  title: 'Astro boilerplate',
+  description: 'Boilerplate built with Astro using React and Tailwind CSS',
+  image: 'https://placehold.co/600x400.png'
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {}
-	},
-	plugins: [
-		require('@tailwindcss/aspect-ratio'),
-		require('@tailwindcss/typography')
-	]
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          primary: {
+            500: '#0B0B0B',
+            400: '#333333',
+            300: '#5C5C5C',
+            200: '#858585',
+            100: '#ADADAD',
+            50: '#D6D6D6'
+          }
+        }
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography')
+  ]
 }
