bd3ab168bf32e0da0820ab13b999de310e211c00
Geovanny Gil <geovannygr2018@gmail.com>
2023-09-27 00:21:56 -0600

    feat: add favicons and manifest config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-	// base: '.', // Set a path prefix
-	site: 'https://example.com',
-	// Use to generate your sitemap and canonical URLs in your final build.
-	integrations: [
-		sitemap(),
-		tailwind(),
-		react()
-	]
+  // base: '.', // Set a path prefix
+  site: 'https://example.com',
+  // Use to generate your sitemap and canonical URLs in your final build.
+  integrations: [
+    sitemap(),
+    tailwind(),
+    react()
+  ]
 })
